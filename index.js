a5d3ad6c61b57bb974b8e050afd70e580b389c1e
jorge_ramos <zramos.ivan@gmail.com>
2023-07-20 21:00:47 -0600

    modifed port for conection

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const databaseUrl = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
+const port = process.env.PORT || 8080
 mongoose
   .connect(databaseUrl)
   .then(() => {
     console.log("Conexión a la base de datos exitosa");
-    app.listen(8080, () => {
+    app.listen(port, () => {
       console.log("Nuestra api de clean arquitecture está prendida!");
     });
     // Levantar servidor
